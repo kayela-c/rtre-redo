bef727423273782e622b1ce0eebc83411a0cdc35
kayela-c <kayela@icloud.com>
2022-07-23 22:39:24 -0500

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,1 @@
 
-
-let cookieModal = document.getElementById('cookies');
-let acceptBtn = document.getElementById('accept-btn');
-
-acceptBtn.addEventListener('click', function(){
-  cookieModal.classList.remove('active')
-  localStorage.setItem('cookieAccepted', 'yes')
-})
-
-setTimeout(function (){
-  let cookieAccepted = localStorage.getItem('cookieAccepted')
-  if (cookieAccepted != 'yes'){
-    cookieModal.classList.add('active')
-  }
-  cookieModal.classList.add('active')
-}, 2000) 
