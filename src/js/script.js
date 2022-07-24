76087a58a89d7cbb4ff1c3369181bcb5870c8e20
kayela-c <kayela@icloud.com>
2022-07-23 21:52:25 -0500

    working on cookie pop up

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "0";
-}
+
+let cookieModal = document.getElementById('cookie-consent-modal');
+let acceptCookieBtn = document.getElementById('accept');
+
+acceptCookieBtn.addEventListener('click', function(){
+  cookieModal.classList.remove('active')
+  localStorage.setItem('cookieAccepted', 'yes')
+})
+
+setTimeout(function (){
+  let cookieAccepted = localStorage.getItem('cookieAccepted')
+  if (cookieAccepted != 'yes'){
+    cookieModal.classList.add('active')
+  }
+  cookieModal.classList.add('active')
+}, 2000) 
