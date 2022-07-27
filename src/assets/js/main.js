6f53d9b0d8236eafe04193950ce5b17b6f903b1e
kayela-c <kayela@icloud.com>
2022-07-27 14:09:59 -0500

    added parcel and moved files

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+import 'bootstrap';
+
+
+// Popup module
+$(document).ready(function(){
+    $('#cookies-modal').modal('show');
+})
+
+// const myModalEl = document.getElementById('cookies-modal')
+// myModalEl.addEventListener('hidden.bs.modal', event => {
+//   // do something...
+// })
